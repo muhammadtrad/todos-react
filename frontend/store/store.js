710d79efeb7f584ab640abe51b8d5f21cfef9325
xmagnetisx <muhammad.trad@yahoo.com>
2020-04-17 19:25:48 -0700

    add middleware

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -1,13 +1,27 @@
- import { createStore } from 'redux';
- import rootReducer from '../reducers/root_reducer';
+import { createStore, applyMiddleware } from 'redux';
+import rootReducer from '../reducers/root_reducer';
 
- const configureStore = (preloadedState = {}) => {
 
-     const store = createStore(rootReducer, preloadedState);
-     store.subscribe( () => {
-         localStorage.state = JSON.stringify(store.getState(), preloadedState);
-     });
-     return store;
- }
+const addLoggingToDispatch = store => next => action => {
+    console.log(store.getState());
+    console.log(action);
+    next(action);
+    console.log(store.getState());
+}
 
-  export default configureStore;
+const anotherMiddleware = store => next => action => {
+    console.log('second middleware');
+    next(action);
+    console.log(action);
+}
+
+const configureStore = (preloadedState = {}) => {
+    const store = createStore(rootReducer, preloadedState, applyMiddleware(addLoggingToDispatch, anotherMiddleware));
+
+    store.subscribe(() => {
+        localStorage.state = JSON.stringify(store.getState());
+    });
+    return store;
+}
+
+export default configureStore;
--- todo_redux.jsx
+++ todo_redux.jsx
@@ -2,8 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Root from './frontend/components/root';
 import configureStore from './frontend/store/store';
-import { receiveTodo, receiveTodos} from './frontend/actions/todo_actions'
-import { allTodos, stepsByTodoId } from './frontend/reducers/selectors';
 
 
 document.addEventListener("DOMContentLoaded", ()=> {
